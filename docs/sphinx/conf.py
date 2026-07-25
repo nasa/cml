c9d4fd0ff5bf12c1f353f6390272c52b77ad5e41
Nino Tarantino <antonio.tarantino@nasa.gov>
2026-07-24 20:21:44 -0500

    Prototype sphinx support
    There's still an issue with (--) Trick units showing up incorrectly in
    Sphinx (not Doxygen).

---
+++ docs/sphinx/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'The Common Model Library'
+copyright = '2026, National Aeronautics and Space Administration'
+author = 'Gary Turner, Nino Tarantino, Dan Jordan'
+release = '0.2.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["breathe", "sphinx.ext.graphviz"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Breathe configuration ---------------------------------------------------
+breathe_default_project = "CML"
+smartquotes = False
+
+# -- Graphviz configuration --------------------------------------------------
+# Breathe hands Doxygen's inheritance graphs to sphinx.ext.graphviz as dot
+# source. SVG keeps them legible at any zoom level; the default is PNG.
+graphviz_output_format = "svg"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
---
+++ docs/sphinx/index.rst
@@ -0,0 +1,11 @@
+The Common Model Library documentation
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. doxygenclass:: SubscriptionBase
+   :members:
+   :protected-members:
+   :undoc-members:
